67ae3b3ee71eb033d64f88c9291b688df11a7034
stutiheble-cloud <stutiheble@gmail.com>
2026-03-27 15:44:56 +0530

    Create addition.cpp
    code

---
+++ addition.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a, b, sum;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+    sum = a + b;
+    cout << "Sum of 2 intergers is : " << sum;
+    return 0;
+}
